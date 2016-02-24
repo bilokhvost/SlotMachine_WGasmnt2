d0b520e90f300b1d1e38529abf65fd8a7ed1f571
Kateryna Bilokhvost <therry26@gmail.com>
2016-02-23 20:27:56 -0500

    event handlers were added; buttons were fixed

--- Scripts/scenes/slotmachine.js
+++ Scripts/scenes/slotmachine.js
@@ -20,16 +20,49 @@ var scenes;
             this.addChild(this._backgroundImage);
             //add bet buttons to the scene
             this._bet5Button = new objects.Button("Bet5Button", 228, 224, false);
+            this.addChild(this._bet5Button);
+            this._bet5Button.on("click", this._bet5ButtonClick, this);
             this._bet10Button = new objects.Button("Bet10Button", 294, 224, false);
+            this.addChild(this._bet10Button);
+            this._bet10Button.on("click", this._bet10ButtonClick, this);
             this._bet100Button = new objects.Button("Bet100Button", 360, 224, false);
+            this.addChild(this._bet100Button);
+            this._bet100Button.on("click", this._bet100ButtonClick, this);
+            //add spin button
             this._spinButton = new objects.Button("SpinButton", 260, 311, false);
+            this.addChild(this._spinButton);
+            this._spinButton.on("click", this._spinButtonClick, this);
+            //add exit and reset buttons
             this._exitButton = new objects.Button("ExitButton", 413, 15, false);
+            this.addChild(this._exitButton);
+            this._exitButton.on("click", this._exitButtonClick, this);
             this._resetButton = new objects.Button("ResetButton", 182, 15, false);
+            this.addChild(this._resetButton);
+            this._resetButton.on("click", this._resetButtonClick, this);
             // add this scene to the global stage container
             stage.addChild(this);
         };
         // SLOT_MACHINE Scene updates here
         SlotMachine.prototype.update = function () {
+        };
+        //EVENT HANDLERS ++++++++++++++++++++
+        SlotMachine.prototype._bet5ButtonClick = function (event) {
+            console.log("bet 5 credits");
+        };
+        SlotMachine.prototype._bet10ButtonClick = function (event) {
+            console.log("bet 10 credits");
+        };
+        SlotMachine.prototype._bet100ButtonClick = function (event) {
+            console.log("bet 100 credits");
+        };
+        SlotMachine.prototype._spinButtonClick = function (event) {
+            console.log("spin");
+        };
+        SlotMachine.prototype._resetButtonClick = function (event) {
+            console.log("reset");
+        };
+        SlotMachine.prototype._exitButtonClick = function (event) {
+            console.log("exit");
         };
         return SlotMachine;
     })(objects.Scene);
--- Scripts/scenes/slotmachine.ts
+++ Scripts/scenes/slotmachine.ts
@@ -3,12 +3,12 @@ module scenes {
     export class SlotMachine extends objects.Scene {
         //PRIVATE INSTANCE VARIABLES ++++++++++++
         private _backgroundImage: createjs.Bitmap;
-        private _bet5Button:objects.Button;
-        private _bet10Button:objects.Button;
-        private _bet100Button:objects.Button;
-        private _spinButton:objects.Button;
-        private _exitButton:objects.Button;
-        private _resetButton:objects.Button;
+        private _bet5Button: objects.Button;
+        private _bet10Button: objects.Button;
+        private _bet100Button: objects.Button;
+        private _spinButton: objects.Button;
+        private _exitButton: objects.Button;
+        private _resetButton: objects.Button;
         
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
@@ -24,12 +24,29 @@ module scenes {
             this.addChild(this._backgroundImage);
             
             //add bet buttons to the scene
-            this._bet5Button = new objects.Button("Bet5Button",228, 224, false);
-            this._bet10Button = new objects.Button("Bet10Button",294, 224, false);
-            this._bet100Button = new objects.Button("Bet100Button",360, 224, false);
-            this._spinButton = new objects.Button("SpinButton",260, 311, false);
-            this._exitButton = new objects.Button("ExitButton",413, 15, false);
-            this._resetButton = new objects.Button("ResetButton",182, 15, false);
+            this._bet5Button = new objects.Button("Bet5Button", 228, 224, false);
+            this.addChild(this._bet5Button);
+            this._bet5Button.on("click", this._bet5ButtonClick, this);
+            this._bet10Button = new objects.Button("Bet10Button", 294, 224, false);
+            this.addChild(this._bet10Button);
+            this._bet10Button.on("click", this._bet10ButtonClick, this);
+            this._bet100Button = new objects.Button("Bet100Button", 360, 224, false);
+            this.addChild(this._bet100Button);
+            this._bet100Button.on("click", this._bet100ButtonClick, this);
+            
+            //add spin button
+            this._spinButton = new objects.Button("SpinButton", 260, 311, false);
+            this.addChild(this._spinButton);
+            this._spinButton.on("click", this._spinButtonClick, this);
+            
+            //add exit and reset buttons
+            this._exitButton = new objects.Button("ExitButton", 413, 15, false);
+            this.addChild(this._exitButton);
+            this._exitButton.on("click", this._exitButtonClick, this);
+            this._resetButton = new objects.Button("ResetButton", 182, 15, false);
+            this.addChild(this._resetButton);
+            this._resetButton.on("click", this._resetButtonClick, this);
+             
             // add this scene to the global stage container
             stage.addChild(this);
         }
@@ -41,6 +58,30 @@ module scenes {
         
         
         //EVENT HANDLERS ++++++++++++++++++++
-        
+        private _bet5ButtonClick(event: createjs.MouseEvent): void {
+            console.log("bet 5 credits");
+        }
+
+        private _bet10ButtonClick(event: createjs.MouseEvent): void {
+            console.log("bet 10 credits");
+        }
+
+        private _bet100ButtonClick(event: createjs.MouseEvent): void {
+            console.log("bet 100 credits");
+        }
+
+        private _spinButtonClick(event: createjs.MouseEvent): void {
+            console.log("spin");
+        }
+
+        private _resetButtonClick(event: createjs.MouseEvent): void {
+            console.log("reset");
+        }
+
+        private _exitButtonClick(event: createjs.MouseEvent): void {
+            console.log("exit");
+        }
+
+
     }
 }
